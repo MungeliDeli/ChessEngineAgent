ebe034118b8ba24cc3886e82e24fd496fa18ded8
Mwansa Davy <119759945+MungeliDeli@users.noreply.github.com>
2024-09-28 12:53:25 +0200

    first committ by mungeli

---
+++ chess/ChessEngine.py
@@ -0,0 +1,26 @@
+'''
+this class keeps information about the state of the game, valid moves and a move log
+
+'''
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+
+        self.whiteMove = True;
+
+class Move():
+    def __init__(self , startSg, endSg, board):
+
+
+
+
